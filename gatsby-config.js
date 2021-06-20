b2ebccd100e9760330d84c7597120e0541387ec5
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-06-20 15:23:49 +0900

    はてなブログの rss を表示するよう追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
-  siteMetadata: {
-    title: "gatsby-porfolio-sample",
-  },
-  plugins: [],
+    siteMetadata: {
+        title: 'gatsby-porfolio-sample',
+        description: 'hello gatsby',
+    },
+    // GraphiQL という GraphQL の IDE が用意されており、
+    // Gatsby の開発サーバを起動すると使用できる
+    // ビルド時に GraphQL で取得したデータも含めて静的化するため本番環境では GraphQL サーバは不要
+    plugins: [
+        {
+            resolve: `gatsby-source-rss-feed`,
+            options: {
+                url: `https://ponkan1219.hateblo.jp/rss`,
+                name: `HatebloIyokan`,
+            },
+        },
+    ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,30 @@
 import * as React from 'react';
+import { graphql } from "gatsby"
 import { Link } from 'gatsby';
 
-const IndexPage = () => {
+const IndexPage = (props) => {
     return (
+      <>
+        {/* JSX 内でオブジェクトを表示できないため、文字列にして表示 */}
+        <pre>{JSON.stringify(props.data, null, 4)}</pre>
         <div>
             <p>Hello gatsby</p>
             <Link to="/about">Link to About</Link>
         </div>
+      </>
     );
 };
+
+export const query = graphql`
+  {
+    allFeedHatebloIyokan {
+      nodes {
+        link
+        pubDate
+        title
+      }
+    }
+  }
+`
 
 export default IndexPage;
