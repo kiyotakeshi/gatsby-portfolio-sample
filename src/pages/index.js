c9f0176855961cdbac88956720ef432157e86dac
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-06-20 15:29:38 +0900

    取得した rss を component として表示

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,46 @@
 import * as React from 'react';
-import { graphql } from "gatsby"
+import { graphql } from 'gatsby';
 import { Link } from 'gatsby';
+
+const HatebloItem = (props) => {
+    const { title, link, pubDate } = props.post;
+    return (
+        <li>
+            <a href={link}>
+                <small>{pubDate}</small> {title}
+            </a>
+        </li>
+    );
+};
 
 const IndexPage = (props) => {
     return (
-      <>
-        {/* JSX 内でオブジェクトを表示できないため、文字列にして表示 */}
-        <pre>{JSON.stringify(props.data, null, 4)}</pre>
-        <div>
-            <p>Hello gatsby</p>
-            <Link to="/about">Link to About</Link>
-        </div>
-      </>
+        <>
+            {/* JSX 内でオブジェクトを表示できないため、文字列にして表示 */}
+            {/* <pre>{JSON.stringify(props.data, null, 4)}</pre> */}
+            <ul>
+                {props.data.allFeedHatebloIyokan.nodes.map((post) => {
+                    return <HatebloItem post={post} key={post.link} />;
+                })}
+            </ul>
+            <div>
+                <p>Hello gatsby</p>
+                <Link to="/about">Link to About</Link>
+            </div>
+        </>
     );
 };
 
 export const query = graphql`
-  {
-    allFeedHatebloIyokan {
-      nodes {
-        link
-        pubDate
-        title
-      }
+    {
+        allFeedHatebloIyokan {
+            nodes {
+                link
+                pubDate
+                title
+            }
+        }
     }
-  }
-`
+`;
 
 export default IndexPage;
